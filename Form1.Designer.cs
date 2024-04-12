5968227f8c506398e6273abe1f544a265f69f088
samuel.varaujo <samuel.varaujo@AME0613343W10-1>
2024-04-11 22:19:12 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace Geradordeimages
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rdBtn_cj = new System.Windows.Forms.RadioButton();
+            this.rdBtn_rider = new System.Windows.Forms.RadioButton();
+            this.rdBtn_big = new System.Windows.Forms.RadioButton();
+            this.rdBtn_kendi = new System.Windows.Forms.RadioButton();
+            this.rdBtn_pol = new System.Windows.Forms.RadioButton();
+            this.rdBtn_matt = new System.Windows.Forms.RadioButton();
+            this.pictureBox_matt = new System.Windows.Forms.PictureBox();
+            this.pictureBox_policial = new System.Windows.Forms.PictureBox();
+            this.pictureBox_kendi = new System.Windows.Forms.PictureBox();
+            this.pictureBox_big = new System.Windows.Forms.PictureBox();
+            this.pictureBox_rider = new System.Windows.Forms.PictureBox();
+            this.pictureBox_cj = new System.Windows.Forms.PictureBox();
+            this.rdBtn_prota = new System.Windows.Forms.RadioButton();
+            this.rdBtn_coad = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_matt)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_policial)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_kendi)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_big)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_rider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_cj)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rdBtn_cj
+            // 
+            this.rdBtn_cj.AutoSize = true;
+            this.rdBtn_cj.Location = new System.Drawing.Point(323, 160);
+            this.rdBtn_cj.Name = "rdBtn_cj";
+            this.rdBtn_cj.Size = new System.Drawing.Size(53, 24);
+            this.rdBtn_cj.TabIndex = 6;
+            this.rdBtn_cj.TabStop = true;
+            this.rdBtn_cj.Text = "CJ";
+            this.rdBtn_cj.UseVisualStyleBackColor = true;
+            this.rdBtn_cj.Visible = false;
+            // 
+            // rdBtn_rider
+            // 
+            this.rdBtn_rider.AutoSize = true;
+            this.rdBtn_rider.Location = new System.Drawing.Point(323, 367);
+            this.rdBtn_rider.Name = "rdBtn_rider";
+            this.rdBtn_rider.Size = new System.Drawing.Size(72, 24);
+            this.rdBtn_rider.TabIndex = 7;
+            this.rdBtn_rider.TabStop = true;
+            this.rdBtn_rider.Text = "Rider";
+            this.rdBtn_rider.UseVisualStyleBackColor = true;
+            this.rdBtn_rider.Visible = false;
+            // 
+            // rdBtn_big
+            // 
+            this.rdBtn_big.AutoSize = true;
+            this.rdBtn_big.Location = new System.Drawing.Point(323, 587);
+            this.rdBtn_big.Name = "rdBtn_big";
+            this.rdBtn_big.Size = new System.Drawing.Size(108, 24);
+            this.rdBtn_big.TabIndex = 8;
+            this.rdBtn_big.TabStop = true;
+            this.rdBtn_big.Text = "Big smoke";
+            this.rdBtn_big.UseVisualStyleBackColor = true;
+            this.rdBtn_big.Visible = false;
+            // 
+            // rdBtn_kendi
+            // 
+            this.rdBtn_kendi.AutoSize = true;
+            this.rdBtn_kendi.Location = new System.Drawing.Point(952, 160);
+            this.rdBtn_kendi.Name = "rdBtn_kendi";
+            this.rdBtn_kendi.Size = new System.Drawing.Size(132, 24);
+            this.rdBtn_kendi.TabIndex = 9;
+            this.rdBtn_kendi.TabStop = true;
+            this.rdBtn_kendi.Text = "kendl johnson";
+            this.rdBtn_kendi.UseVisualStyleBackColor = true;
+            this.rdBtn_kendi.Visible = false;
+            // 
+            // rdBtn_pol
+            // 
+            this.rdBtn_pol.AutoSize = true;
+            this.rdBtn_pol.Location = new System.Drawing.Point(952, 366);
+            this.rdBtn_pol.Name = "rdBtn_pol";
+            this.rdBtn_pol.Size = new System.Drawing.Size(82, 24);
+            this.rdBtn_pol.TabIndex = 10;
+            this.rdBtn_pol.TabStop = true;
+            this.rdBtn_pol.Text = "Policial";
+            this.rdBtn_pol.UseVisualStyleBackColor = true;
+            this.rdBtn_pol.Visible = false;
+            // 
+            // rdBtn_matt
+            // 
+            this.rdBtn_matt.AutoSize = true;
+            this.rdBtn_matt.Location = new System.Drawing.Point(952, 586);
+            this.rdBtn_matt.Name = "rdBtn_matt";
+            this.rdBtn_matt.Size = new System.Drawing.Size(97, 24);
+            this.rdBtn_matt.TabIndex = 11;
+            this.rdBtn_matt.TabStop = true;
+            this.rdBtn_matt.Text = "Matt dog";
+            this.rdBtn_matt.UseVisualStyleBackColor = true;
+            this.rdBtn_matt.Visible = false;
+            // 
+            // pictureBox_matt
+            // 
+            this.pictureBox_matt.Image = global::Geradordeimages.Properties.Resources.mad;
+            this.pictureBox_matt.Location = new System.Drawing.Point(645, 507);
+            this.pictureBox_matt.Name = "pictureBox_matt";
+            this.pictureBox_matt.Size = new System.Drawing.Size(289, 178);
+            this.pictureBox_matt.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_matt.TabIndex = 5;
+            this.pictureBox_matt.TabStop = false;
+            this.pictureBox_matt.Visible = false;
+            // 
+            // pictureBox_policial
+            // 
+            this.pictureBox_policial.Image = global::Geradordeimages.Properties.Resources.pol;
+            this.pictureBox_policial.Location = new System.Drawing.Point(645, 302);
+            this.pictureBox_policial.Name = "pictureBox_policial";
+            this.pictureBox_policial.Size = new System.Drawing.Size(289, 166);
+            this.pictureBox_policial.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_policial.TabIndex = 4;
+            this.pictureBox_policial.TabStop = false;
+            this.pictureBox_policial.Visible = false;
+            // 
+            // pictureBox_kendi
+            // 
+            this.pictureBox_kendi.Image = global::Geradordeimages.Properties.Resources.kendi;
+            this.pictureBox_kendi.Location = new System.Drawing.Point(645, 85);
+            this.pictureBox_kendi.Name = "pictureBox_kendi";
+            this.pictureBox_kendi.Size = new System.Drawing.Size(289, 171);
+            this.pictureBox_kendi.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_kendi.TabIndex = 3;
+            this.pictureBox_kendi.TabStop = false;
+            this.pictureBox_kendi.Visible = false;
+            // 
+            // pictureBox_big
+            // 
+            this.pictureBox_big.Image = global::Geradordeimages.Properties.Resources.big;
+            this.pictureBox_big.Location = new System.Drawing.Point(31, 507);
+            this.pictureBox_big.Name = "pictureBox_big";
+            this.pictureBox_big.Size = new System.Drawing.Size(285, 178);
+            this.pictureBox_big.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_big.TabIndex = 2;
+            this.pictureBox_big.TabStop = false;
+            // 
+            // pictureBox_rider
+            // 
+            this.pictureBox_rider.Image = global::Geradordeimages.Properties.Resources.rider;
+            this.pictureBox_rider.Location = new System.Drawing.Point(31, 302);
+            this.pictureBox_rider.Name = "pictureBox_rider";
+            this.pictureBox_rider.Size = new System.Drawing.Size(285, 166);
+            this.pictureBox_rider.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_rider.TabIndex = 1;
+            this.pictureBox_rider.TabStop = false;
+            // 
+            // pictureBox_cj
+            // 
+            this.pictureBox_cj.Image = global::Geradordeimages.Properties.Resources.cj;
+            this.pictureBox_cj.Location = new System.Drawing.Point(31, 85);
+            this.pictureBox_cj.Name = "pictureBox_cj";
+            this.pictureBox_cj.Size = new System.Drawing.Size(285, 171);
+            this.pictureBox_cj.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_cj.TabIndex = 0;
+            this.pictureBox_cj.TabStop = false;
+            // 
+            // rdBtn_prota
+            // 
+            this.rdBtn_prota.AutoSize = true;
+            this.rdBtn_prota.Location = new System.Drawing.Point(109, 44);
+            this.rdBtn_prota.Name = "rdBtn_prota";
+            this.rdBtn_prota.Size = new System.Drawing.Size(124, 24);
+            this.rdBtn_prota.TabIndex = 12;
+            this.rdBtn_prota.TabStop = true;
+            this.rdBtn_prota.Text = "Protagonista";
+            this.rdBtn_prota.UseVisualStyleBackColor = true;
+            this.rdBtn_prota.CheckedChanged += new System.EventHandler(this.rdBtn_prota_CheckedChanged);
+            // 
+            // rdBtn_coad
+            // 
+            this.rdBtn_coad.AutoSize = true;
+            this.rdBtn_coad.Location = new System.Drawing.Point(738, 44);
+            this.rdBtn_coad.Name = "rdBtn_coad";
+            this.rdBtn_coad.Size = new System.Drawing.Size(126, 24);
+            this.rdBtn_coad.TabIndex = 13;
+            this.rdBtn_coad.TabStop = true;
+            this.rdBtn_coad.Text = "Coadijuvante";
+            this.rdBtn_coad.UseVisualStyleBackColor = true;
+            this.rdBtn_coad.CheckedChanged += new System.EventHandler(this.rdBtn_coad_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1181, 714);
+            this.Controls.Add(this.rdBtn_coad);
+            this.Controls.Add(this.rdBtn_prota);
+            this.Controls.Add(this.rdBtn_matt);
+            this.Controls.Add(this.rdBtn_pol);
+            this.Controls.Add(this.rdBtn_kendi);
+            this.Controls.Add(this.rdBtn_big);
+            this.Controls.Add(this.rdBtn_rider);
+            this.Controls.Add(this.rdBtn_cj);
+            this.Controls.Add(this.pictureBox_matt);
+            this.Controls.Add(this.pictureBox_policial);
+            this.Controls.Add(this.pictureBox_kendi);
+            this.Controls.Add(this.pictureBox_big);
+            this.Controls.Add(this.pictureBox_rider);
+            this.Controls.Add(this.pictureBox_cj);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_matt)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_policial)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_kendi)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_big)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_rider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_cj)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox_cj;
+        private System.Windows.Forms.PictureBox pictureBox_rider;
+        private System.Windows.Forms.PictureBox pictureBox_big;
+        private System.Windows.Forms.PictureBox pictureBox_kendi;
+        private System.Windows.Forms.PictureBox pictureBox_policial;
+        private System.Windows.Forms.PictureBox pictureBox_matt;
+        private System.Windows.Forms.RadioButton rdBtn_cj;
+        private System.Windows.Forms.RadioButton rdBtn_rider;
+        private System.Windows.Forms.RadioButton rdBtn_big;
+        private System.Windows.Forms.RadioButton rdBtn_kendi;
+        private System.Windows.Forms.RadioButton rdBtn_pol;
+        private System.Windows.Forms.RadioButton rdBtn_matt;
+        private System.Windows.Forms.RadioButton rdBtn_prota;
+        private System.Windows.Forms.RadioButton rdBtn_coad;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Geradordeimages
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void rdBtn_prota_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox_cj.Visible = true;
+            pictureBox_big.Visible = true;
+            pictureBox_rider.Visible = true;
+        }
+
+        private void rdBtn_coad_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox_kendi.Visible = true;
+            pictureBox_policial.Visible = true;
+            pictureBox_matt.Visible = true;
+        }
+    }
+}
